b449ea4b403b27831640e2c509a8a4b35b3eec56
Mistzin07 <arthurviniciossoaresbezerra@gmail.com>
2024-05-18 11:47:16 -0300

    Fech e Get na API do GitHub

---
+++ main.js
@@ -0,0 +1,25 @@
+const img = document.querySelector('.img')
+const titulo = document.querySelector('.titulo')
+const subtitulo = document.querySelector('.sub-titulo')
+const bio = document.querySelector('.bio')
+const qtdSeguidores = document.querySelector('.qtd-seguidores')
+const qtdSeguindo = document.querySelector('.qtd-seguindo')
+const qtdRepositorios = document.querySelector('.qtd-repositorios')
+
+ const url = 'https://api.github.com/users/Mistzin07'
+const getData = async() =>{ 
+    const response =await fetch(url)
+    const data =await response.json()
+    img.setAttribute('src','data.avatar_url')
+    console.log(data.bio)
+    titulo.innerHTML = data.name
+    subtitulo.innerHTML = data.login
+    bio.innerHTML = data.bio
+    qtdSeguidores.innerHTML = data.followers
+    qtdSeguindo.innerHTML = data.following
+    qtdRepositorios.innerHTML = data.public_repos
+
+}
+getData()
+
+
